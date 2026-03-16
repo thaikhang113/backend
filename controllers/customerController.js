3edc4191ca9c4380cb42808af85bcf46dd50058f
root <root@vmi2918028.contaboserver.net>
2026-03-16 02:40:02 +0100

    Feature: Add update password, check user by id

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,15 +1,101 @@
 const db = require('../config/db');
 const crypto = require('crypto');
 
+const updatePassword = async (req, res) => {
+    try {
+
+        const { email, newPassword } = req.body;
+
+        if (!email || !newPassword) {
+            return res.status(400).json({
+                message: "Email and new password are required"
+            });
+        }
+
+        const result = await db.query(
+            "UPDATE Users SET password_hash = $1 WHERE email = $2 AND is_staff = FALSE RETURNING user_id, email",
+            [newPassword, email]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                message: "Customer not found"
+            });
+        }
+
+        res.json({
+            message: "Password updated successfully",
+            user: result.rows[0]
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        });
+    }
+};
+const getCustomerByEmail = async (req, res) => {
+    try {
+        const { email } = req.params;
+
+        const result = await db.query(
+            'SELECT user_id, username, email, first_name, last_name, phone_number, address, date_of_birth, is_active FROM Users WHERE email = $1 AND is_staff = FALSE',
+            [email]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                message: "Customer not found"
+            });
+        }
+
+        res.json(result.rows[0]);
+
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        });
+    }
+};
 const getAllCustomers = async (req, res) => {
     try {
-        const result = await db.query('SELECT user_id, username, email, first_name, last_name, phone_number, address, date_of_birth, is_active FROM Users WHERE is_staff = FALSE');
+        const result = await db.query('SELECT user_id, username, email,password_hash, first_name, last_name, phone_number, address, date_of_birth, is_active FROM Users WHERE is_staff = FALSE');
         res.json(result.rows);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
 };
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "Email and password are required"
+            });
+        }
+
+        const result = await db.query(
+            "SELECT * FROM Users WHERE email = $1 AND password_hash = $2",
+            [email, password]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(401).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        res.json({
+            message: "Login successful"
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        });
+    }
+};
 const getCustomerById = async (req, res) => {
     try {
         const { id } = req.params;
@@ -22,14 +108,14 @@ const getCustomerById = async (req, res) => {
 };
 
 const createCustomer = async (req, res) => {
-    const { username, password, email, first_name, last_name, phone_number, address, date_of_birth, gender } = req.body;
+    const { username, password, email, first_name, last_name, phone_number, address, date_of_birth } = req.body;
     try {
-        const passwordHash = crypto.createHash('sha256').update(password).digest('hex');
+        const passwordHash = password;
         const query = `
             INSERT INTO Users (username, password_hash, email, first_name, last_name, phone_number, address, date_of_birth, gender, is_staff)
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, FALSE) RETURNING user_id, username;
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8,'male', FALSE) RETURNING user_id, username;
         `;
-        const result = await db.query(query, [username, passwordHash, email, first_name, last_name, phone_number, address, date_of_birth, gender]);
+        const result = await db.query(query, [username, passwordHash, email, first_name, last_name, phone_number, address, date_of_birth]);
         res.status(201).json({ message: 'Customer created', user: result.rows[0] });
     } catch (err) {
         res.status(500).json({ error: err.message });
@@ -63,4 +149,4 @@ const deleteCustomer = async (req, res) => {
     }
 };
 
-module.exports = { getAllCustomers, getCustomerById, createCustomer, updateCustomer, deleteCustomer };
+module.exports = {updatePassword, getCustomerByEmail, getAllCustomers,login, getCustomerById, createCustomer, updateCustomer, deleteCustomer };
--- routes/customers.js
+++ routes/customers.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const customerController = require('../controllers/customerController');
 
+
+router.put('/update-password',customerController.updatePassword);
 router.get('/', customerController.getAllCustomers);
 // Ai cũng có thể tạo tài khoản (Register) - Không cần auth
 router.post('/', customerController.createCustomer); 
 // Endpoint mo hoan toan, khong can auth
+router.get('/email/:email',customerController.getCustomerByEmail);
 router.get('/:id', customerController.getCustomerById);
 router.put('/:id', customerController.updateCustomer);
 router.delete('/:id', customerController.deleteCustomer);
-
+router.post('/login', customerController.login);
 module.exports = router;
